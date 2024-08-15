1643a1e89a572edc48c148d2e16e268d631854d9
Durga1805 <durgasreenivas2003@gmail.com>
2024-08-15 13:30:55 +0530

    starting over

--- LIBRARY_MANAGEMENT_SYSTEM/lms/src/App.js
+++ LIBRARY_MANAGEMENT_SYSTEM/lms/src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
-  return (
-    <div>hello</div>
+  return (<>
+  
+  
+    <Header/>
+    <main className='min-h-[calc(100vh-60px)] pt-16'>
+    <Outlet/>
+    </main>
+    <Footer/>
     
-  );
+    </> );
 }
 
 export default App;
--- LIBRARY_MANAGEMENT_SYSTEM/lms/src/routes/index.js
+++ LIBRARY_MANAGEMENT_SYSTEM/lms/src/routes/index.js
@@ -1,2 +1,42 @@
-import React from "react";
+import { createBrowserRouter } from 'react-router-dom'
+import App from '../App'
+import Home from '../pages/Home'
+import Login from '../pages/Login'
 
+
+
+
+const router = createBrowserRouter([
+    {
+        path : "/",
+        element : <App/>,
+        children : [
+            {
+                path : "",
+                element : <Home/>
+            },
+            {
+                path : "login",
+                element : <Login/>
+            },
+            
+         
+        
+            // {
+            //     path : "forgot-password",
+            //     element : <ForgotPassowrd/>
+            // },
+            // {
+            //     path : "sign-up",
+            //     element : <SignUp/>
+            // },
+            
+
+
+
+
+        ]
+    }
+])
+
+export default router
